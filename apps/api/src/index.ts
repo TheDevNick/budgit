07500d9a42ec4824126218ac2e002ce457e8d658
Nick <nick@rivialsecurity.com>
2024-12-20 02:37:32 -0600

    feat: initialize project

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
-import { log } from "@repo/logger";
 import { createServer } from "./server";
 
 const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
-  log(`api running on ${port}`);
+  // eslint-disable-next-line no-console
+  console.log(`api running on ${port}`);
 });
---
+++ apps/web/src/app/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function App(): JSX.Element {
+  return <div className="container">budgit</div>;
+}
+
+export default App;
