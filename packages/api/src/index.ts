96618deab3979c262776488af3a4bbf8e667979a
Nick <nick@rivialsecurity.com>
2024-12-24 14:55:59 -0600

    chore: update jest config

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,5 +4,5 @@ const PORT = process.env.PORT ?? 4000;
 const server = createServer();
 
 server.listen(PORT, () => {
-  console.log(`🚀 API running on ${PORT} ✅`);
+  console.log(`🚀 API running on http://localhost:${PORT} ✅`);
 });
